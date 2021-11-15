a8c1e5faaa2781694c43f81973d95ea6b7bcb87e
Rafa Dominguez <rafa.realmadrid.rdm@gmail.com>
2021-11-15 10:52:20 +0100

    Primer commit

---
+++ src/main/java/com/clientemicroservicio/clientemicroservicio/ClientemicroservicioApplication.java
@@ -0,0 +1,13 @@
+package com.clientemicroservicio.clientemicroservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientemicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientemicroservicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clientemicroservicio/clientemicroservicio/ClientemicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientemicroservicio.clientemicroservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientemicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
